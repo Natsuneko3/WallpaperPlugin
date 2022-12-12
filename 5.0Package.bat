19d0ee112842cad1ef92f1e87d2c1872df70f2ad
Natsuneko <kon3nian2ban@gmail.com>
2022-12-12 22:14:04 +0800

    Natsuneko

--- 5.0Package.bat
+++ 5.0Package.bat
@@ -1,8 +1,8 @@
 c:
 cd C:\Users\Neko\OneDrive\UePlugins\WallpaperEnginePlugin\Content
 IF EXIST "Cache" rd /s /q Cache
-f:
-cd F:\UE_5.0\Engine\Build\BatchFiles
+D:
+cd D:\Epic Games\UE_5.0\Engine\Build\BatchFiles
 call RunUAT.bat BuildPlugin -Plugin="C:\Users\Neko\OneDrive\UePlugins\WallpaperEnginePlugin\Wallpaper.uplugin" -Package="C:\Users\Neko\OneDrive\UePlugins\WallpaperEnginePlugin\Package\UE5.0\Wallpaper" -Rocket
 c:
 cd C:\Users\Neko\OneDrive\UePlugins\WallpaperEnginePlugin\Package\UE5.0\Wallpaper
