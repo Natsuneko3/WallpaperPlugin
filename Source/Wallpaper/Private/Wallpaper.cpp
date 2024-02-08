800bca97ad77a17832eb3c0c5e05d9ae7524ecbc
Natsuneko3 <70563504+Natsuneko3@users.noreply.github.com>
2024-02-08 11:42:02 +0800

    Update Wallpaper.cpp

--- Source/Wallpaper/Private/Wallpaper.cpp
+++ Source/Wallpaper/Private/Wallpaper.cpp
@@ -112,17 +112,12 @@ void FWallPaperModule::ShutdownModule()
 	
 	StyleSettings->ResetStyleColor();
 	//Clear Cache
-	FString PluginsPath = FPaths::ProjectPluginsDir()/"Wallpaper";
-	if(!IFileManager::Get().DirectoryExists(*PluginsPath))
-    	{
-    		PluginsPath = FPaths::EnginePluginsDir()/"Wallpaper";
-    	}
-    		
-    	FString TargetFilePath = PluginsPath/"Content/Cache";
-    	if(IFileManager::Get().DirectoryExists(*TargetFilePath))
-    	{
-    		IFileManager::Get().DeleteDirectory(*(TargetFilePath),false,true);
-    	}
+
+	FString TargetFilePath = FPaths::ProjectSavedDir()/"Cache";
+	if(IFileManager::Get().DirectoryExists(*TargetFilePath))
+	{
+		IFileManager::Get().DeleteDirectory(*TargetFilePath,false,true);
+	}
 }
 
 bool FWallPaperModule::OnSettingModified()
@@ -617,7 +612,7 @@ void FWallPaperModule::ImportPicTheme()
 		PluginsPath = FPaths::EnginePluginsDir()/"Marketplace/Wallpaper";
 	}
 		
-	FString TargetFilePath = PluginsPath/"Content/Cache";
+	FString TargetFilePath = FPaths::ProjectSavedDir()/"Cache";
 	//clear file
 	if(IFileManager::Get().DirectoryExists(*TargetFilePath))
 	{
